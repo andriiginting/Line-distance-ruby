b78d5e885f3eb555949afe76c6a47522806834e4
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-06-29 08:27:11 +0700

    add compare 2 line spec

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -3,4 +3,10 @@ require "./lib/line.rb"
 describe "Line" do
   it  "initial test" do
   end
+
+  it "can compare this end point" do
+    first_line = Line::Line.new(3,3,0,0)
+    second_line = Line::Line.new(3,3,0,0)
+    expect(0).to eq(0)
+  end
 end
