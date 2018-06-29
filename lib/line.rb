d5c55df7403b3415651139a1eaed35ef8fe4acbd
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-06-29 08:25:24 +0700

    add line initiliaze

--- lib/line.rb
+++ lib/line.rb
@@ -1,6 +1,12 @@
 # Your code goes here...
 module Line
   class Line
-    
+    attr_reader :x1,:x2,:y1,:y2
+      def initialize(x1,y1,x2,y2)
+          @x1 = x1
+          @y1 = y1
+          @x2 = x2
+          @y2 = y2
+      end
   end
 end  
