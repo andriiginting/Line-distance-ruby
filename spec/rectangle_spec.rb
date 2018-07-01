dd01284f22a0c426dc220f23a5fe7b97de92844f
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-01 22:33:11 +0700

    Add spec to create rectangle with negative length

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -19,4 +19,8 @@ describe "Rectangle" do
     rectangle = Geometry::Rectangle.new(0,0,-10,0)
     expect(0).to eq(rectangle.get_perimeter)
   end
+  it  "can create a rectangle with negative length" do
+    rectangle = Geometry::Rectangle.new(0,0,0,-10)
+    expect(0).to eq(rectangle.get_perimeter)
+  end
 end
