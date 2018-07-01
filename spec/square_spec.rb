090a8f313f094c1da61e153a9c5002fdc9a04670
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-01 22:45:06 +0700

    Add spec create square with zero length

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -3,7 +3,12 @@ require './lib/square'
 
 describe "Square" do
   it  "can create a square" do
+    rectangle = Geometry::Square.new(0,0,5)
+    expect(20).to eq(rectangle.get_perimeter)
+  end
+
+  it  "can create a square with zero length" do
     rectangle = Geometry::Square.new(0,0,0)
-    expect(0).to eq(0)
+    expect(0).to eq(rectangle.get_perimeter)
   end
 end
