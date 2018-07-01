aab18ddbe76e1e1df2cdabef84395dac33f8167e
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-01 22:32:00 +0700

    add spec create rectangle with zero width

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -9,4 +9,9 @@ describe "Rectangle" do
     rectangle = Geometry::Rectangle.new(0,0,0,10)
     expect(0).to eq(rectangle.get_perimeter)
   end
+
+  it  "can create a rectangle with zero width" do
+    rectangle = Geometry::Rectangle.new(0,0,10,0)
+    expect(0).to eq(rectangle.get_perimeter)
+  end
 end
