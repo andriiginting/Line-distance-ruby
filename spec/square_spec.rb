b2eec5cc70fbcb576ab56ce3c9e4d95acae9bc7d
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-01 22:45:58 +0700

    Add spec create square with negative value

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -11,4 +11,9 @@ describe "Square" do
     rectangle = Geometry::Square.new(0,0,0)
     expect(0).to eq(rectangle.get_perimeter)
   end
+
+  it  "can create a square with negative length" do
+    rectangle = Geometry::Square.new(0,0,-10)
+    expect(40).to eq(rectangle.get_perimeter)
+  end
 end
