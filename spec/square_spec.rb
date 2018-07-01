28906ce97913cb336d60b531e1bafb03594aeb66
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-01 22:46:52 +0700

    Add spec to get area of square

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -16,4 +16,9 @@ describe "Square" do
     rectangle = Geometry::Square.new(0,0,-10)
     expect(40).to eq(rectangle.get_perimeter)
   end
+
+  it  "can calculate area of square" do
+    rectangle = Geometry::Square.new(0,0,0)
+    expect(0).to eq(rectangle.get_area)
+  end
 end
